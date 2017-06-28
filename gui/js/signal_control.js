ca0317463abfde1bbb3814aa260f8d42c0fd0b34
Minu <minuapple@gmail.com>
2017-06-28 16:35:34 +0530

    displaying and adding groups

--- gui/js/signal_control.js
+++ gui/js/signal_control.js
@@ -125,100 +125,108 @@ $( document ).ready(function() {
 		type: 'POST',
 		success: function(result) {
 			var signals = jQuery.parseJSON(result)
+			// console.log(signals);
 			for (var i = 0; i < signals.length; i++) {
-				$('.group_table tbody').append('<tr><td colspan="1"><input type="radio" name="groups"></td><td colspan="2" group_id='+signals[i].GroupID+'>'+(i+1)+'</td><td colspan="4">'+signals[i].GroupName+'</td><td colspan="4">'+signals[i].NumPlans+'</td></tr>')
+				$('.group_table tbody').append('<tr><td colspan="1"><input type="radio" name="groups"></td><td colspan="2" group_id='+signals[i].GroupID+'>'+(i+1)+'</td><td colspan="4">'+signals[i].Name+'</td><td colspan="4">'+signals[i].SCN+'</td></tr>')
 			}
 		}
 	});
 	$('.add_group').click(function(){
 		var group_name = $('.group_name').val()
+		var group_scn = $('.group_scn').val()
+		var group_description = $('.group_description').val()
 		if(group_name == ""){
 			alert("Please Enter Group Name");
 			return;
 		}
-		var num_plans = $('.num_plans_drop').find(":selected").text();
-		var post_times = []
-		var time_arrays = []
-		for(var i=0;i<num_plans;i++){
-			var element = document.getElementsByClassName('plan_config_body')[0].rows[i]
-			var plan_no = element.cells[0].innerHTML
-			var start_time = $(element.cells[1]).find(":selected").text()
-			var end_time = $(element.cells[2]).find(":selected").text()
-			if(start_time == end_time){
-				alert("Start Time and End Time cannot be Equal")
-				return;
-			}
-			var start_datetime = new Date('2014','8','2',start_time.split(':')[0],start_time.split(':')[1])
-			var end_datetime = new Date('2014','8','2',end_time.split(':')[0],end_time.split(':')[1])
-			if(end_datetime < start_datetime){
-				alert("End Time cannot be less than Start Time")
-				return;
-			}
-			time_arrays.push([start_datetime, end_datetime])
-			post_times.push([start_time, end_time])
-		}
-		var check = false;
-		for (var i = 0; i < time_arrays.length; i++) {
-			for (var j = 0; j < time_arrays.length; j++) {
-				if(i!=j){
-					if(time_arrays[i][0] < time_arrays[j][0] && time_arrays[i][1] > time_arrays[j][0]){
-						check = true;
-					}
-					if(time_arrays[i][0] > time_arrays[j][1] && time_arrays[i][1] < time_arrays[j][1]){
-						check = true;
-					}
-				}
-			}
-		}
-		if(check){
-			alert("Plan Overlapped, Please Retry")
-			return;
-		}
-		var time_config = 0;
-		for (var i = 0; i < time_arrays.length; i++) {
-			time_config += time_arrays[i][1]-time_arrays[i][0]
-		}
-		time_config = time_config/3600000
-		if(time_config != 24){ 
-			alert("Please Schedule the group for 24 hr.");
-			return;
-		}
+		// var num_plans = $('.num_plans_drop').find(":selected").text();
+		// var post_times = []
+		// var time_arrays = []
+		// for(var i=0;i<num_plans;i++){
+		// 	var element = document.getElementsByClassName('plan_config_body')[0].rows[i]
+		// 	var plan_no = element.cells[0].innerHTML
+		// 	var start_time = $(element.cells[1]).find(":selected").text()
+		// 	var end_time = $(element.cells[2]).find(":selected").text()
+		// 	if(start_time == end_time){
+		// 		alert("Start Time and End Time cannot be Equal")
+		// 		return;
+		// 	}
+		// 	var start_datetime = new Date('2014','8','2',start_time.split(':')[0],start_time.split(':')[1])
+		// 	var end_datetime = new Date('2014','8','2',end_time.split(':')[0],end_time.split(':')[1])
+		// 	if(end_datetime < start_datetime){
+		// 		alert("End Time cannot be less than Start Time")
+		// 		return;
+		// 	}
+		// 	time_arrays.push([start_datetime, end_datetime])
+		// 	post_times.push([start_time, end_time])
+		// }
+		// var check = false;
+		// for (var i = 0; i < time_arrays.length; i++) {
+		// 	for (var j = 0; j < time_arrays.length; j++) {
+		// 		if(i!=j){
+		// 			if(time_arrays[i][0] < time_arrays[j][0] && time_arrays[i][1] > time_arrays[j][0]){
+		// 				check = true;
+		// 			}
+		// 			if(time_arrays[i][0] > time_arrays[j][1] && time_arrays[i][1] < time_arrays[j][1]){
+		// 				check = true;
+		// 			}
+		// 		}
+		// 	}
+		// }
+		// if(check){
+		// 	alert("Plan Overlapped, Please Retry")
+		// 	return;
+		// }
+		// var time_config = 0;
+		// for (var i = 0; i < time_arrays.length; i++) {
+		// 	time_config += time_arrays[i][1]-time_arrays[i][0]
+		// }
+		// time_config = time_config/3600000
+		// if(time_config != 24){ 
+		// 	alert("Please Schedule the group for 24 hr.");
+		// 	return;
+		// }
 		$.ajax({
 			url: '../utils/check_groupname.php',
-			data :{group_name:group_name},
+			data :{group_scn: group_scn},
 			type: 'POST',
 			success: function(result) {
+				console.log(result);
 				if(result != 0){
-					alert("Group Name Already Exists try again.");
-					$('.group_name').val("")
-					$('.group_name').focus();
+					alert("Group SCN Already Exists try again.");
+					$('.group_scn').val("")
+					$('.group_scn').focus();
 					return;
 				}
 				else{
 					var message = "Group has been added successfully";
-					insert_group(group_name, num_plans, post_times, message);
+					insert_group(group_name, group_scn, group_description, message);
 				}
 			}
 		});
 	});
-	insert_group = function(group_name, num_plans, post_times, message){
-		var string = '{'
-		for (var i = 0; i < post_times.length; i++) {
-			string += '"'+i+'":'+'{'
-			string += '"order":"'+(i+1)+'",'
-			string += '"start":"'+post_times[i][0]+'",'
-			string += '"end":"'+post_times[i][1]+'"'
-			if(i == post_times.length-1){
-				string += '}'
-			}
-			else{
-				string += '},'
-			}
-		}
-		string += '}'
+	insert_group = function(group_name, group_scn, group_description, message){
+		// var string = '{'
+		// for (var i = 0; i < post_times.length; i++) {
+		// 	string += '"'+i+'":'+'{'
+		// 	string += '"order":"'+(i+1)+'",'
+		// 	string += '"start":"'+post_times[i][0]+'",'
+		// 	string += '"end":"'+post_times[i][1]+'"'
+		// 	if(i == post_times.length-1){
+		// 		string += '}'
+		// 	}
+		// 	else{
+		// 		string += '},'
+		// 	}
+		// }
+		// string += '}'
 		$.ajax({
 			url: '../utils/add_group.php',
-			data :{group_name:group_name,num_plans:num_plans,string:string},
+			data :{
+				group_name: group_name,
+				group_scn: group_scn,
+				group_description: group_description
+			},
 			type: 'POST',
 			success: function(result) {
 				if(result.includes("success")){
