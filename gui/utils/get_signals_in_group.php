f85db066267c584bb8cd471b554a9c8f5641a7c5
raghuramsirigiri <raghuram9758@gmail.com>
2017-06-29 16:11:34 +0530

    modified get all signals in group

--- gui/utils/get_signals_in_group.php
+++ gui/utils/get_signals_in_group.php
@@ -8,7 +8,15 @@ try {
     $handle=$link->prepare("SELECT  `SignalID` ,  `SCN` ,  `ShortDescription` FROM  `utmc_traffic_signal_static` WHERE `Group_SCN` = :group_scn "); 
     $handle->bindParam(':group_scn', $group_scn);
     $handle->execute();
-	$result=$handle->fetchall(PDO::FETCH_ASSOC);
+    $result = array();
+    while ($row = $handle->fetch(PDO::FETCH_ASSOC)){
+    	$handle2=$link->prepare("SELECT * FROM `utmc_traffic_signal_plans_stages` WHERE `SignalID` = :signalid"); 
+		$handle2->bindParam(':signalid', $row["SignalID"]);
+		$handle2->execute();
+		$count = $handle2->fetchall(PDO::FETCH_ASSOC);
+		$result_row = ['SignalID'=>$row["SignalID"],'SCN'=>$row["SCN"],'ShortDescription'=>$row["ShortDescription"],'StagesNumber'=>count($count)];
+		array_push($result, $result_row);
+    }
 	return json_encode($result);
 }
 
