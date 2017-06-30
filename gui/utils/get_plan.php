f969372c4e8865beb123bb79d5e12a197a2c50b5
raghuramsirigiri <raghuram9758@gmail.com>
2017-06-30 11:52:01 +0530

    added get plan list

---
+++ gui/utils/get_plan.php
@@ -0,0 +1,28 @@
+<?php
+include 'dblinker.php';
+
+function get_plan(){
+try {
+	$plan_scn = $_POST['plan_scn'];
+	$link = linkToTIS();
+    $handle=$link->prepare("SELECT  `SignalID` ,  `SCN` ,  `ShortDescription` FROM  `utmc_traffic_signal_static` WHERE `Group_SCN` = :group_scn "); 
+    $handle->bindParam(':group_scn', $group_scn);
+    $handle->execute();
+    $result = array();
+    while ($row = $handle->fetch(PDO::FETCH_ASSOC)){
+    	$handle2=$link->prepare("SELECT * FROM `utmc_traffic_signal_plans_stages` WHERE `SignalID` = :signalid"); 
+		$handle2->bindParam(':signalid', $row["SignalID"]);
+		$handle2->execute();
+		$count = $handle2->fetchall(PDO::FETCH_ASSOC);
+		$result_row = ['SignalID'=>$row["SignalID"],'SCN'=>$row["SCN"],'ShortDescription'=>$row["ShortDescription"],'StagesNumber'=>count($count)];
+		array_push($result, $result_row);
+    }
+	return json_encode($result);
+}
+
+catch(Exception $e){
+        return "F";
+    }
+}
+echo get_plan();
+?>
--- gui/utils/get_plan_list.php
+++ gui/utils/get_plan_list.php
@@ -4,11 +4,13 @@ include 'dblinker.php';
 
 function get_plan_list(){
 	try {
+		$group_scn = $_POST['group_scn'];
 		$link = linkToTIS();
-	    $handle=$link->prepare("SELECT * FROM plans"); 
+	    $handle=$link->prepare("SELECT * FROM `plans` WHERE `Group_SCN` = :group_scn"); 
+	    $handle->bindParam(':group_scn', $group_scn);
 	    $handle->execute();
 
-	    $plan_list= $handle->fetchall(PDO::FETCH_ASSOC);
+	    $plan_list= $handle->dfetchall(PDO::FETCH_ASSOC);
 	    
 	    return json_encode($plan_list);
 	}
