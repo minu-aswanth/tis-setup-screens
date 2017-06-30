e5b2877f143ccb14e1db3350a6b1dcb552f41fb8
raghuramsirigiri <raghuram9758@gmail.com>
2017-06-30 17:41:43 +0530

    added get plan

--- gui/utils/add_calendar.php
+++ gui/utils/add_calendar.php
@@ -6,46 +6,23 @@ function add_calender(){
 	try {
 		$calendar = json_decode($_POST['calendar']);
 		$group_scn = $_POST['group_scn'];
+		print_r($calendar);
 		
 		$link = linkToTIS();
 		foreach ($calendar as $day) {
 			$date = $day->day;
 			$timetable_scn = $day->timetable_scn;
-
-		}
-		$handle=$link->prepare("INSERT INTO `plans`(`PlanSCN`, `Group_SCN`, `CycleTime`) VALUES (:plan_scn, :group_scn, :cycle_time)");
-		$handle->bindParam(':group_scn', $group_scn);
-		$handle->bindParam(':plan_scn', $plan_scn);
-		$handle->bindParam(':cycle_time', $cycle_time);
-		$handle->execute();
-
-		
-		foreach ($signals as $signal) {
-			$signal_scn = $signal->signal_scn;
-			$signal_id = $signal->signal_id;
-			$i = 1;
-			foreach ($signal->timings as $time) {
-				$handle=$link->prepare("INSERT INTO `signal_timings`(`SignalID`, `SignalSCN`, `Plan_SCN`, `StageNumber`, `StageTime`) VALUES (:signal_id, :signal_scn, :plan_scn, :stage_number, :stage_time)");
-				$handle->bindParam(':signal_id', $signal_id);
-				$handle->bindParam(':signal_scn', $signal_scn);
-				$handle->bindParam(':plan_scn', $plan_scn);
-				$handle->bindParam(':stage_number', $i);
-				$handle->bindParam(':stage_time', $time);
+			foreach ($day->plan_info as $plan) {
+				$handle=$link->prepare("INSERT INTO `calendar`(`Date`, `Group_SCN`, `TimeTable_SCN`, `Slot_Order`, `Plan_SCN`) VALUES (:date_day, :group_scn, :timetable_scn, :slot_order, :plan_scn )");
+				$handle->bindParam(':date_day', $date);
+				$handle->bindParam(':group_scn', $group_scn);
+				$handle->bindParam(':timetable_scn', $timetable_scn);
+				$handle->bindParam(':slot_order', $plan->slot_order);
+				$handle->bindParam(':plan_scn', $plan->plan_scn);
 				$handle->execute();
-				$i++;
 			}
-		}
 
-		$offsets = json_decode($_POST['offsets']);
-		foreach ($offsets as $offset) {
-			$handle=$link->prepare("INSERT INTO `offsets`(`Plan_SCN`, `Origin_Signal_SCN`, `Destination_Signal_SCN`, `OffsetTime`) VALUES (:plan_scn, :origin_signal_scn, :destination_signal_scn, :offset_time)");
-			$handle->bindParam(':plan_scn', $plan_scn);
-			$handle->bindParam(':origin_signal_scn', $offset->start_signal_scn);
-			$handle->bindParam(':destination_signal_scn', $offset->end_signal_scn);
-			$handle->bindParam(':offset_time', $offset->offset_time);
-			$handle->execute();
 		}
-
 		return "success";
 	}
 	catch(Exception $e){
---
+++ gui/utils/get_calendar.php
@@ -0,0 +1,32 @@
+<?php
+include 'dblinker.php';
+
+function get_signal_scns(){
+try {
+	$group_scn = $_POST['group_scn'];
+	
+    $link = linkToTIS();
+    $handle=$link->prepare("SELECT DISTINCT Date,TimeTable_SCN FROM `calendar` WHERE `Group_SCN` = :group_scn ");
+    $handle->bindParam(':group_scn', $group_scn);
+    $handle->execute();
+
+    $result = array();
+    while ($row = $handle->fetch(PDO::FETCH_ASSOC)){
+        $handle2 = $link->prepare("SELECT calendar. * , time_tables.StartTime, time_tables.EndTime FROM calendar INNER JOIN time_tables ON (calendar.TimeTable_SCN = time_tables.TimeTableSCN AND calendar.Slot_Order = time_tables.SlotOrder) AND (calendar.Date = :date_day AND calendar.Group_SCN = :group_scn)"); 
+        $handle2->bindParam(':date_day', $row["Date"]);
+        $handle2->bindParam(':group_scn', $group_scn);
+        $handle2->execute();
+
+        $day_object = array("day"=>$row["Date"],"timetable_scn"=>$row["TimeTable_SCN"],"plan_info"=>$handle2->fetchall(PDO::FETCH_ASSOC));
+        array_push($result, $day_object);
+    }
+
+    return json_encode($result);
+}
+
+catch(Exception $e){
+        return "F";
+    }
+}
+echo get_signal_scns();
+?>
