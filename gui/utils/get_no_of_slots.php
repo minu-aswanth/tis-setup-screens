6d22e1dab7c4b741323cb6e79c0e638add056407
raghuramsirigiri <raghuram9758@gmail.com>
2017-06-30 15:52:41 +0530

    added slot count of timetables

--- gui/utils/get_no_of_slots.php
+++ gui/utils/get_no_of_slots.php
@@ -5,12 +5,14 @@ function get_timetable_slot_number(){
 try {
 	$timetable_scn = $_POST['timetable_scn'];
 	$link = linkToTIS();
-    $handle=$link->prepare("SELECT * FROM `time_tables` WHERE `TimeTableSCN` = :timetable_scn "); 
+    $handle=$link->prepare("SELECT `StartTime`,`EndTime` FROM `time_tables` WHERE `TimeTableSCN` = :timetable_scn "); 
     $handle->bindParam(':timetable_scn', $timetable_scn);
     $handle->execute();
-    $result = $handle->fetchall(PDO::FETCH_ASSOC);
+    $slots_array = $handle->fetchall(PDO::FETCH_ASSOC);
 
-	return json_encode(count($result));
+    $result = array('count'=>count($slots_array),'slots'=>$slots_array);
+
+	return json_encode($result);
 }
 
 catch(Exception $e){
