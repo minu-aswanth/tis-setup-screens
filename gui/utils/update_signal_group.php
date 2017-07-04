6fdc4bef7a0ea9d9262af174abbf80b0f7efa1b0
raghuramsirigiri <raghuram9758@gmail.com>
2017-07-04 16:45:28 +0530

    updated add signal with plans

--- gui/utils/update_signal_group.php
+++ gui/utils/update_signal_group.php
@@ -11,6 +11,37 @@ function update_group(){
 		$handle->bindParam(':group_scn', $group_scn);
 		$handle->bindParam(':signal_scn', $signal_scn);
 		$handle->execute();
+
+		$handle=$link->prepare("SELECT SignalID FROM `utmc_traffic_signal_static` WHERE `SCN`= :signal_scn");
+		$handle->bindParam(':signal_scn', $signal_scn);
+		$handle->execute();
+		$row = $handle->fetch(PDO::FETCH_ASSOC);
+		$signal_id = $row["SignalID"];
+
+		$plan_info = json_decode($_POST['signal_scn']);
+		foreach ($plan_info as $plan) {
+			$plan_scn = $plan->plan_scn;
+			$i = 1;
+			foreach ($plan->timings as $time) {
+				$handle=$link->prepare("INSERT INTO `signal_timings`(`SignalID`, `SignalSCN`, `Plan_SCN`, `StageNumber`, `StageTime`) VALUES (:signal_id, :signal_scn, :plan_scn, :stage_number, :stage_time)");
+				$handle->bindParam(':signal_id', $signal_id);
+				$handle->bindParam(':signal_scn', $signal_scn);
+				$handle->bindParam(':plan_scn', $plan_scn);
+				$handle->bindParam(':stage_number', $i);
+				$handle->bindParam(':stage_time', $time);
+				$handle->execute();
+				$i++;
+			}
+
+			foreach ($plan_info->offset_info as $offset) {
+				$handle=$link->prepare("INSERT INTO `offsets`(`Plan_SCN`, `Origin_Signal_SCN`, `Destination_Signal_SCN`, `OffsetTime`) VALUES (:plan_scn, :origin_signal_scn, :destination_signal_scn, :offset_time)");
+				$handle->bindParam(':plan_scn', $plan_scn);
+				$handle->bindParam(':origin_signal_scn', $offset->start_signal_scn);
+				$handle->bindParam(':destination_signal_scn', $offset->end_signal_scn);
+				$handle->bindParam(':offset_time', $offset->offset_time);
+				$handle->execute();
+			}
+		}
 		
 		return "success";
 	}
