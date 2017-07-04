7f267e8eae3446a826cc8b6ee81cb81976611db1
raghuramsirigiri <raghuram9758@gmail.com>
2017-07-04 15:41:01 +0530

    modified plan list with offset

---
+++ gui/utils/get_no_of_stages.php
@@ -0,0 +1,23 @@
+<?php
+include 'dblinker.php';
+
+function get_no_of_stages(){
+try {
+	$signal_scn = $_POST['signal_scn'];
+
+	$link = linkToTIS();
+    $handle=$link->prepare("SELECT utmc_traffic_signal_plans_stages.StageID
+	FROM `utmc_traffic_signal_plans_stages`
+	INNER JOIN `utmc_traffic_signal_static` ON utmc_traffic_signal_static.SignalID = utmc_traffic_signal_plans_stages.SignalID AND utmc_traffic_signal_plans_stages.SCN = :signal_scn"); 
+	$handle->bindParam(':signal_scn', $signal_scn);
+    $handle->execute();
+	$result=$handle->fetchall(PDO::FETCH_ASSOC);
+	return json_encode(count($result));
+}
+
+catch(Exception $e){
+        return "F";
+    }
+}
+echo get_no_of_stages();
+?>
--- gui/utils/get_plan_list.php
+++ gui/utils/get_plan_list.php
@@ -10,8 +10,17 @@ function get_plan_list(){
 	    $handle->bindParam(':group_scn', $group_scn);
 	    $handle->execute();
 
-	    $plan_list= $handle->fetchall(PDO::FETCH_ASSOC);
+	    $plan_list= array();
+	    while ($row = $handle->fetch(PDO::FETCH_ASSOC)){
+	    	$handle2=$link->prepare("SELECT * FROM `offsets` WHERE `Plan_SCN` = :plan_scn"); 
+		    $handle2->bindParam(':plan_scn', $row["PlanSCN"]);
+		    $handle2->execute();
+
+		    $plan_object = array("PlanSCN"=>$row["PlanSCN"],"CycleTime"=>$row["CycleTime"],"Offsets"=>$handle2->fetchall(PDO::FETCH_ASSOC));
+		    array_push($plan_list, $plan_object);
+	    }
 	    
+
 	    return json_encode($plan_list);
 	}
 	catch(Exception $e){
