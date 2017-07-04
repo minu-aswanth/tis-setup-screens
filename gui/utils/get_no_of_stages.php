e702a92593987e0c1ce8ff116572fe7ea48c6cf0
raghuramsirigiri <raghuram9758@gmail.com>
2017-07-04 16:23:29 +0530

    added get no of stages

--- gui/utils/get_no_of_stages.php
+++ gui/utils/get_no_of_stages.php
@@ -6,12 +6,11 @@ try {
 	$signal_scn = $_POST['signal_scn'];
 
 	$link = linkToTIS();
-    $handle=$link->prepare("SELECT utmc_traffic_signal_plans_stages.StageID
-	FROM `utmc_traffic_signal_plans_stages`
-	INNER JOIN `utmc_traffic_signal_static` ON utmc_traffic_signal_static.SignalID = utmc_traffic_signal_plans_stages.SignalID AND utmc_traffic_signal_plans_stages.SCN = :signal_scn"); 
-	$handle->bindParam(':signal_scn', $signal_scn);
+    $handle=$link->prepare("SELECT utmc_traffic_signal_plans_stages.StageID, utmc_traffic_signal_plans_stages.SignalID	FROM `utmc_traffic_signal_plans_stages`	INNER JOIN `utmc_traffic_signal_static` ON (utmc_traffic_signal_static.SignalID = utmc_traffic_signal_plans_stages.SignalID AND utmc_traffic_signal_static.SCN = :signal_scn )");
+    $handle->bindParam(':signal_scn', $signal_scn);
     $handle->execute();
-	$result=$handle->fetchall(PDO::FETCH_ASSOC);
+    $result	= $handle->fetchall(PDO::FETCH_ASSOC);
+	
 	return json_encode(count($result));
 }
 
