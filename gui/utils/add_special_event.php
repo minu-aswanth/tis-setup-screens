80cbb35e30ed04e5b16b0f5b7030149e35ff313e
raghuramsirigiri <raghuram9758@gmail.com>
2017-07-04 12:00:01 +0530

    added add special event

--- gui/utils/add_special_event.php
+++ gui/utils/add_special_event.php
@@ -6,23 +6,24 @@ function add_special_event(){
 	try {
 		$calendar = json_decode($_POST['calendar']);
 		$group_scn = $_POST['group_scn'];
-		print_r($calendar);
+		$event_date = $_POST['event_date'];
+		$timetable_scn = $_POST['timetable_scn'];
 		
 		$link = linkToTIS();
-		foreach ($calendar as $day) {
-			$date = $day->day;
-			$timetable_scn = $day->timetable_scn;
-			foreach ($day->plan_info as $plan) {
-				$handle=$link->prepare("INSERT INTO `calendar`(`Date`, `Group_SCN`, `TimeTable_SCN`, `Slot_Order`, `Plan_SCN`) VALUES (:date_day, :group_scn, :timetable_scn, :slot_order, :plan_scn )");
-				$handle->bindParam(':date_day', $date);
-				$handle->bindParam(':group_scn', $group_scn);
-				$handle->bindParam(':timetable_scn', $timetable_scn);
-				$handle->bindParam(':slot_order', $plan->slot_order);
-				$handle->bindParam(':plan_scn', $plan->plan_scn);
-				$handle->execute();
-			}
+	
+		foreach ($calendar as $plan) {
+			$handle=$link->prepare("INSERT INTO `special_event` (`Date`, `Group_SCN`, `TimeTable_SCN`, `slot_order`, `Plan_SCN`) VALUES (:date_day, :group_scn, :timetable_scn, :slot_order, :plan_scn )");
+			$handle->bindParam(':date_day', $event_date);
+			$handle->bindParam(':group_scn', $group_scn);
+			$handle->bindParam(':timetable_scn', $timetable_scn);
+			$handle->bindParam(':slot_order', $plan->slot_order);
+			$handle->bindParam(':plan_scn', $plan->plan_scn);
+			$handle->execute();
+			print_r($plan);
 
 		}
+
+	
 		return "success";
 	}
 	catch(Exception $e){
